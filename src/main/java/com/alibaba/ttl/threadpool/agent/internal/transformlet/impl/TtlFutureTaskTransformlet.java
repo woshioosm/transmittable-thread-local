2a74f47c8ff063ddbdd9bd9353f8fea9fccc71a5
shuimiao <shuimia0915@163.com>
2019-04-17 15:04:25 +0800

    add isAPI check

--- src/main/java/com/alibaba/ttl/threadpool/agent/internal/transformlet/impl/TtlFutureTaskTransformlet.java
+++ src/main/java/com/alibaba/ttl/threadpool/agent/internal/transformlet/impl/TtlFutureTaskTransformlet.java
@@ -1,5 +1,6 @@
 package com.alibaba.ttl.threadpool.agent.internal.transformlet.impl;
 
+import com.alibaba.ttl.TtlCallable;
 import com.alibaba.ttl.threadpool.agent.internal.logging.Logger;
 import com.alibaba.ttl.threadpool.agent.internal.transformlet.JavassistTransformlet;
 import javassist.*;
@@ -29,27 +30,29 @@ public class TtlFutureTaskTransformlet implements JavassistTransformlet {
                 clazz.addField(f);
                 CtField f2 = CtField.make("private Object backup;", clazz);
                 clazz.addField(f2);
+                CtField f3 = CtField.make("private boolean isApiTtl;", clazz);
+                clazz.addField(f3);
 
 
                 // 更新 构造方法
                 CtConstructor[] constructMethods = clazz.getConstructors();
                 for (CtConstructor ctMethod : constructMethods) {
-                    String code = "capturedRef = new java.util.concurrent.atomic.AtomicReference(com.alibaba.ttl.TransmittableThreadLocal.Transmitter.capture());";
-                    ctMethod.insertBefore(code);
-                }
+                    String consCode = "capturedRef = new java.util.concurrent.atomic.AtomicReference(com.alibaba.ttl.TransmittableThreadLocal.Transmitter.capture());";
+                    String consCode2 = "isApiTtl=($1 instanceof com.alibaba.ttl.TtlCallable || $1 instanceof com.alibaba.ttl.TtlRunnable)?true:false;";
 
+                    ctMethod.insertBefore(consCode + consCode2);
+                }
 
                 // 更新 run方法
                 CtMethod method = clazz.getDeclaredMethod("run");
 
 
+                String code1 = "if(!isApiTtl){";
                 String code2 = "Object captured = capturedRef.get();";
-                String code3 = "backup = com.alibaba.ttl.TransmittableThreadLocal.Transmitter.replay(captured);";
-                final String beforeCode = code2 + code3;
-//                method.insertBefore(code2 + code3);
+                String code3 = "backup = com.alibaba.ttl.TransmittableThreadLocal.Transmitter.replay(captured);}";
+                final String beforeCode = code1 + code2 + code3;
 //
-                final String afterCode = "com.alibaba.ttl.TransmittableThreadLocal.Transmitter.restore(backup);";
-//                method.insertAfter(code4);
+                final String afterCode = "if(!isApiTtl){com.alibaba.ttl.TransmittableThreadLocal.Transmitter.restore(backup);}";
 
 
                 method.instrument(new ExprEditor() {
